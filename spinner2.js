dc3048cd6c6589a133c3ea2606e45c7212d8e47c
Aiman03at <aiman03at@gmail.com>
2024-06-08 20:57:45 -0400

    Refavtoring the Spinner functionality using loops

--- spinner2.js
+++ spinner2.js
@@ -1,13 +1,11 @@
 ////Refactoring the spinner1.js code to have lesser no of steps that are not repetetive.
 
 
-const spinner=()=>{
-  for (let i=100; i <= 1500; i=i+200) {
-    setTimeout(() => {
-      process.stdout.write('\r   '); 
-    }, i);
-    
-  }
+const symbols = ['|', '/', '-', '\\', '|', '/', '-', '\\'];
+const delay = 200; // delay between each symbol in milliseconds
 
-}
- spinner();
+symbols.forEach((symbol, index) => {
+  setTimeout(() => {
+    process.stdout.write(`\r${symbol}   `);
+  }, delay * index);
+});
